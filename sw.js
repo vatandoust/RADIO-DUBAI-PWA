9c2f579e591603ef9ea66c56716f07b921cd9c89
vatandoust <58954130+vatandoust@users.noreply.github.com>
2025-11-17 09:21:36 +0400

    Update service worker caching logic and names

--- sw.js
+++ sw.js
@@ -1,11 +1,18 @@
-const CACHE='radio-shell-v1';
-const ASSETS=['./','./index.html','./styles.css','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{
-  const url=new URL(e.request.url);
-  if(ASSETS.some(a=>url.pathname.endsWith(a.replace('./','/')))){
-    e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request)));return;
-  }
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+const CACHE_NAME = 'roc-dubai-v3';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/styles.css',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
